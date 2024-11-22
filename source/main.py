d8508f1b600c24c866946dc083ee4f2112d965e0
Tiago Lourinho <tiagolourinho001@gmail.com>
2024-11-22 00:56:58 +0000

    added imports so bot is aware of commands

--- source/main.py
+++ source/main.py
@@ -2,6 +2,9 @@ import os
 
 # Commands and events aren't used but need to be imported to register
 from discord_bot.bot import bot
+from discord_bot.events import *
+from discord_bot.commands import *
+from discord_bot.tasks import *
 from dotenv import load_dotenv
 
 load_dotenv()
